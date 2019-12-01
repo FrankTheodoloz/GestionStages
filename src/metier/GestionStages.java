e6b6be706eacb2a5ce2d26fdd7d4f239f480aeae
Frank Théodoloz <frank@16d.ch>
2019-12-01 22:23:27 +0100

    afficherLesStagesAvecDu

--- src/metier/GestionStages.java
+++ src/metier/GestionStages.java
@@ -3,6 +3,7 @@ package metier;
 import domaine.*;
 
 import java.util.ArrayList;
+
 import static dao.StageDao.chargerStages;
 
 public class GestionStages {
@@ -13,7 +14,20 @@ public class GestionStages {
      * @param nom indique le nom de l'activité à rechercher. */
     private static void afficherLesStagesAvecDu(String nom) {
         System.out.println("Liste des stages avec du " + nom + " :");
-        // ===== À CODER =====
+        for (Stage stage : stages) {
+
+            if (stage instanceof StageMulti) {
+                for (Activite activite : ((StageMulti) stage).getActivites()) {
+                    if (activite.getNom().equals(nom)) {
+                        System.out.println(stage);
+                    }
+                }
+            } else {
+                if (((StageSport) stage).getActivite().getNom().equals(nom)) {
+                    System.out.println(stage);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
